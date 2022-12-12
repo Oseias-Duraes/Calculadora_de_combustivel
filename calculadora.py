1b4fd07b51eebc4570bf9a462884a2a4969e002a
Oseias-Duraes <oseias-duraes@users.noreply.github.com>
2022-12-12 20:55:38 -0300

    Alterações no método calcular 12-12-2022

--- calculadora.py
+++ calculadora.py
@@ -6,14 +6,14 @@ class Calculadora:
 
     def calcular_total_gasolina (self,km, preco_gasolina):
         self.total_gasolina = km * preco_gasolina
-        ## return self.total_gasolina -- não necessário pois o print está no main.py
+        ## return self.total_gasolina -- não necessário pois o print está no main
 
 
     def calcular_total_etanol(self,km, preco_etanol):
         self.total_etanol = km * preco_etanol
-        ## return self.total_etanol -- não necessário pois o print está no main.py
+        ## return self.total_etanol -- não necessário pois o print está no main
 
 
     def calcular_total_diesel(self,km, preco_diesel):
         self.total_diesel = km * preco_diesel
-        ## return self.total_diesel -- não necessário pois o print está no main.py
+        ## return self.total_diesel -- não necessário pois o print está no main
